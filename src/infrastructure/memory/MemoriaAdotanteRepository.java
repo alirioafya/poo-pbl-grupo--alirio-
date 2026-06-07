e6f6be86da34e3dd4b31803ce493212bf10ee945
alirioafya <aliriobooks@gmail.com>
2026-06-07 14:39:44 -0300

    Create MemoriaAdotanteRepository.java

---
+++ src/infrastructure/memory/MemoriaAdotanteRepository.java
@@ -0,0 +1,35 @@
+package infrastructure.memory;
+
+import domain.adotante.Adotante;
+import infrastructure.repositories.AdotanteRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MemoriaAdotanteRepository implements AdotanteRepository {
+
+private final List<Adotante> adotantes = new ArrayList<>();
+
+@Override
+public void salvar(Adotante adotante) {
+adotantes.add(adotante);
+}
+
+@Override
+public Optional<Adotante> buscarPorId(Long id) {
+return adotantes.stream()
+.filter(a -> a.getId().equals(id))
+.findFirst();
+}
+
+@Override
+public List<Adotante> listarTodos() {
+return new ArrayList<>(adotantes);
+}
+
+@Override
+public void remover(Long id) {
+adotantes.removeIf(a -> a.getId().equals(id));
+}
+}
